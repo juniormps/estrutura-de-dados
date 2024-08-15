4a4fa6564503e947e78d459f800e70252436b7ff
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-08-15 20:22:18 -0300

    update filas

--- exemplos/fila1.js
+++ exemplos/fila1.js
@@ -1,58 +1,57 @@
-//Implementação de uma Fila
+//Implementação de uma Fila Dinâmica
 
 class Fila {
-    constructor() {
-      this.itens = []
-    }
-  
-    // Adiciona um elemento no final da fila  (ENFILEIRAR)
-    enqueue(elemento) {
-      this.itens.push(elemento);
-    }
-  
-    // Remove o elemento do início da fila (DESENFILEIRAR)
-    dequeue() {
-      if (this.isEmpty()) {
-        return "A fila está vazia";
-      }
-      return this.itens.shift();
-    }
-  
-    // Retorna o elemento no início da fila sem removê-lo
-    front() {
-      if (this.isEmpty()) {
-        return "A fila está vazia";
-      }
-      return this.itens[0];
-    }
-  
-    // Verifica se a fila está vazia
-    isEmpty() {
-      return this.itens.length === 0;
-    }
-  
-    // Retorna o tamanho da fila
-    tamanho() {
-      return this.itens.length;
-    }
-  
-    // Limpa a fila
-    limpar() {
-      this.itens = [];
-    }
+  constructor() {
+    this.itens = []
   }
-  
-  // Testando a fila
-  const fila = new Fila();
-  fila.enqueue(10)
-  fila.enqueue(20)
-  fila.enqueue(30)
-  console.log(fila)
-  console.log(fila.front())
-  fila.dequeue()
-  console.log(fila)
-  console.log(fila.tamanho())
-  fila.enqueue(40)
-  console.log(fila)
-  console.log(fila.tamanho())
-  
+
+  // Adiciona um elemento no final da fila  (ENFILEIRAR)
+  enqueue(elemento) {
+    this.itens.push(elemento)
+  }
+
+  // Remove o elemento do início da fila (DESENFILEIRAR)
+  dequeue() {
+    if (this.isEmpty()) {
+      return "A fila está vazia"
+    }
+    return this.itens.shift()
+  }
+
+  // Retorna o elemento no início da fila sem removê-lo
+  front() {
+    if (this.isEmpty()) {
+      return "A fila está vazia"
+    }
+    return this.itens[0]
+  }
+
+  // Verifica se a fila está vazia
+  isEmpty() {
+    return this.itens.length === 0
+  }
+
+  // Retorna o tamanho da fila
+  tamanho() {
+    return this.itens.length
+  }
+
+  // Limpa a fila
+  limpar() {
+    this.itens = []
+  }
+}
+
+// Testando a fila
+const fila = new Fila()
+fila.enqueue(10)
+fila.enqueue(20)
+fila.enqueue(30)
+console.log(fila)
+console.log(fila.front())
+fila.dequeue()
+console.log(fila)
+console.log(fila.tamanho())
+fila.enqueue(40)
+console.log(fila)
+console.log(fila.tamanho())
--- exemplos/fila2.js
+++ exemplos/fila2.js
@@ -1,71 +1,75 @@
-//Implementação de uma Fila
+//Implementação de uma Fila Estática
 
 class Fila {
     constructor(tamanhoMaximo = 5) {
-      this.itens = []
-      this.tamanhoMaximo = tamanhoMaximo
+        this.itens = []
+        this.tamanhoMaximo = tamanhoMaximo
     }
-  
+
     // Adiciona um elemento no final da fila  (ENFILEIRAR)
     enqueue(elemento) {
-      if (this.itens.length < this.tamanhoMaximo) {
-        return this.itens.push(elemento);
-      } else {
-        return "A fila está cheia." 
-      }
-      
+        if (this.isFull()) {
+            console.log("A fila está cheia")
+        }
+        return this.itens.push(elemento)
+
     }
-  
+
     // Remove o elemento do início da fila (DESENFILEIRAR)
     dequeue() {
-      if (this.isEmpty()) {
-        console.log("A fila está vazia")
-      }
-      return this.itens.shift();
+        if (this.isEmpty()) {
+            console.log("A fila está vazia")
+        }
+        return this.itens.shift()
     }
-  
+
     // Retorna o elemento no início da fila sem removê-lo
     front() {
-      if (this.isEmpty()) {
-        return "A fila está vazia";
-      }
-      return this.itens[0];
+        if (this.isEmpty()) {
+            return "A fila está vazia"
+        }
+        return this.itens[0];
     }
-  
+
     // Verifica se a fila está vazia
     isEmpty() {
-      return this.itens.length === 0;
+        return this.itens.length === 0
     }
-  
+
+    //Verifica se a fila está cheia
+    isFull() {
+        return this.itens.length >= this.tamanhoMaximo
+    }
+
     // Retorna o tamanho da fila
     tamanho() {
-      return this.itens.length;
+        return this.itens.length
     }
-  
+
     // Limpa a fila
     limpar() {
-      this.itens = [];
+        this.itens = []
     }
-  }
-  
-  // Testando a fila
-  const fila = new Fila();
-  fila.enqueue(10)
-  fila.enqueue(20)
-  fila.enqueue(30)
-  fila.enqueue(40)
-  fila.enqueue(50)
-  console.log(fila)
-  console.log(fila.front())
-  fila.dequeue()
-  console.log(fila)
-  console.log(fila.tamanho())
-  fila.enqueue(60)
-  console.log(fila)
-  console.log(fila.tamanho())
-  console.log(fila)
-  fila.enqueue(70)
-  console.log(fila)
-  fila.limpar()
-  console.log(fila)
-  fila.dequeue()
+}
+
+// Testando a fila
+const fila = new Fila()
+fila.enqueue(10)
+fila.enqueue(20)
+fila.enqueue(30)
+fila.enqueue(40)
+fila.enqueue(50)
+console.log(fila)
+console.log(fila.front())
+fila.dequeue()
+console.log(fila)
+console.log(fila.tamanho())
+fila.enqueue(60)
+console.log(fila)
+console.log(fila.tamanho())
+console.log(fila)
+fila.enqueue(70)
+console.log(fila)
+fila.limpar()
+console.log(fila)
+fila.dequeue()
