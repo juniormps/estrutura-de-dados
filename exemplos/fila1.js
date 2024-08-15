6aad805c29f7afa971188be820ea87a0d0110650
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-08-15 19:22:45 -0300

    Create fila1.js

---
+++ exemplos/fila1.js
@@ -0,0 +1,52 @@
+//Implementação de uma Fila
+
+class Fila {
+    constructor() {
+      this.itens = [];
+    }
+  
+    // Adiciona um elemento no final da pilha
+    push(elemento) {
+      this.itens.push(elemento);
+    }
+  
+    // Remove e retorna o elemento do início da fila
+    pop() {
+      if (this.isEmpty()) {
+        return "A pilha está vazia";
+      }
+      return this.itens.pop();
+    }
+  
+    // Retorna o elemento no topo sem removê-lo
+    peek() {
+      if (this.isEmpty()) {
+        return "A pilha está vazia";
+      }
+      return this.itens[this.itens.length - 1];
+    }
+  
+    // Verifica se a pilha está vazia
+    isEmpty() {
+      return this.itens.length === 0;
+    }
+  
+    // Retorna o tamanho da pilha
+    tamanho() {
+      return this.itens.length;
+    }
+  
+    // Limpa a pilha
+    limpar() {
+      this.itens = [];
+    }
+  }
+  
+  // Testando a pilha
+  const pilha = new Pilha();
+  pilha.push(10);  // Adiciona 10
+  pilha.push(20);  // Adiciona 20
+  console.log(pilha.peek());  // Retorna 20
+  pilha.pop();  // Remove o 20
+  console.log(pilha.tamanho());  // Retorna 1
+  
